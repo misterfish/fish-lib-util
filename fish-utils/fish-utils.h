b05859ddd07c81f22379d3a5b92b528c8c3d258d
mister fish <me@mister-fish.net>
2015-02-03 13:03:44 +0100

    d

--- fish-utils/fish-utils.h
+++ fish-utils/fish-utils.h
@@ -7,22 +7,26 @@
 #include <string.h>
 #include <stdlib.h>
 
+/* DEBUG is globally exported. Niet handig. XX
+ */
+
+/*
 // be polite
 #ifdef DEBUG
  #define SAVE_DEBUG
 #else
  #define SAVE_UNDEBUG
 #endif
+*/
 
 /* Define/undef here:
-
 #define DEBUG
-
  */
 
 
 #include <fish-util.h>
 
+/*
 #ifdef SAVE_DEBUG
  #define DEBUG
 #endif
@@ -30,6 +34,7 @@
 #ifdef SAVE_UNDEBUG
  #undef DEBUG
 #endif
+*/
 
 #include "fish-utils/vec.h"
 #include "fish-utils/regex.h"
